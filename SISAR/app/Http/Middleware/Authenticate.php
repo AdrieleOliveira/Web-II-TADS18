eaedf339d827520eac0df530464fb1583b41f31b
AdrieleOliveira <adrisantosdeoliveira@gmail.com>
2021-02-28 19:06:40 -0300

    Att

--- SISAR/app/Http/Middleware/Authenticate.php
+++ SISAR/app/Http/Middleware/Authenticate.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Illuminate\Support\Facades\Log;
 
 class Authenticate extends Middleware
 {
@@ -18,25 +19,5 @@ class Authenticate extends Middleware
         if (! $request->expectsJson()) {
             return route('login');
         }
-    }
-
-    public function handle($request, Closure $next)
-    {
-//        $nivel = 1;
-//        $rota = $request->route()->getName();
-//
-        $response = $next($request);
-//
-//        if($rota != "negado") {
-//            if($rota != "home"){
-//                if ($nivel == 0) {
-//                    return redirect('negado');
-//                } else if($nivel == 1 && ($rota != "curso.index") && $rota != "disciplina.index"){
-//                    return redirect('negado');
-//                }
-//            }
-//        }
-
-        return $response;
     }
 }
--- SISAR/routes/web.php
+++ SISAR/routes/web.php
@@ -3,8 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Middleware\AccessLevel;
 
-
-Route::middleware('auth', 'AccessLevel')->group(function (){
+Route::middleware(['auth', 'AccessLevel'])->group(function (){
     Route::resource('curso', 'CursoController');
     Route::resource('disciplina', 'DisciplinaController');
     Route::resource('professor', 'ProfessorController');
